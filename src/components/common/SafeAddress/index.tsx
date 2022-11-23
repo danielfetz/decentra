22db13ca408274f711b8ad120c86311f21100a03
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-11-23 16:05:18 +0100

    Update index.tsx

--- src/components/common/SafeAddress/index.tsx
+++ src/components/common/SafeAddress/index.tsx
@@ -1,15 +1,11 @@
 import type { ReactElement } from 'react'
-import { useEffect, useState } from 'react'
 import Typography from '@mui/material/Typography'
 import type { IconButtonProps } from '@mui/material/IconButton'
-import IconButton from '@mui/material/IconButton'
 import Skeleton from '@mui/material/Skeleton'
-import Tooltip from '@mui/material/Tooltip'
 
 import { formatCurrency } from '@/utils/formatNumber'
 import useSafeInfo from '@/hooks/useSafeInfo'
 import SafeIcon from '@/components/common/SafeIcon'
-import NewTxButton from '@/components/sidebar/NewTxButton'
 import useBalances from '@/hooks/useBalances'
 import { useAppSelector } from '@/store'
 import { selectCurrency } from '@/store/settingsSlice'
@@ -19,9 +15,6 @@ import css from './styles.module.css'
 import { selectSettings } from '@/store/settingsSlice'
 import { useCurrentChain } from '@/hooks/useChains'
 import EthHashInfo from '@/components/common/EthHashInfo'
-import Track from '@/components/common/Track'
-import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
-import { SvgIcon } from '@mui/material'
 
 const SafeAddress = (): ReactElement => {
   const currency = useAppSelector(selectCurrency)
