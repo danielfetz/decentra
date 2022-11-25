471b2ea3f13e206966a740b67155420b957a228d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-11-25 16:03:47 +0100

    Update routes.ts

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -7,6 +7,7 @@ export const AppRoutes = {
   import: '/import',
   home: '/home',
   apps: '/apps',
+  txbuilder: '/apps?appUrl=https%3A%2F%2Fapps.gnosis-safe.io%2Ftx-builder',
   addressBook: '/address-book',
   balances: {
     nfts: '/balances/nfts',
