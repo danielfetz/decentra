4289193798ef65ffbda84d16709b7a6117fb12c1
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-11-26 13:21:43 +0100

    Update SafeAppsDashboardSection.tsx

--- src/components/dashboard/SafeAppsDashboardSection/SafeAppsDashboardSection.tsx
+++ src/components/dashboard/SafeAppsDashboardSection/SafeAppsDashboardSection.tsx
@@ -1,16 +1,32 @@
 import { useRouter } from 'next/router'
-import Typography from '@mui/material/Typography'
-import Grid from '@mui/material/Grid'
+import { Grid, Skeleton, Typography } from '@mui/material'
 
 import { Card, WidgetContainer } from '../styled'
 import { AppCard } from '@/components/safe-apps/FeaturedAppCard'
 import { useSafeApps } from '@/hooks/safe-apps/useSafeApps'
+
+const ValueSkeleton = () => <Skeleton variant="text" width={30} />
+
+const SkeletonOverview = (
+  <Grid container>
+    <Grid item xs={12} sm={6} md={3} xl={3}>
+      <Card>
+        <Typography variant="h5">
+                <Skeleton variant="text" height={28} />
+        </Typography>
+      </Card>
+    </Grid>
+  </Grid>
+  )
 
 const SafeAppsDashboardSection = () => {
   const { rankedSafeApps, pinnedSafeAppIds } = useSafeApps()
 
   return (
     <WidgetContainer>
+      {safeLoading ? (
+          SkeletonOverview
+        ) : (
       <Grid container spacing={3}>
         {rankedSafeApps.map((rankedSafeApp) => (
           <Grid key={rankedSafeApp.id} item xs={12} sm={6} md={3} xl={3}>
@@ -22,6 +38,7 @@ const SafeAppsDashboardSection = () => {
           </Grid>
         ))}
       </Grid>
+        )}
     </WidgetContainer>
   )
 }
