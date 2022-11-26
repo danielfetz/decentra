c86c55e152f530c705877aab7ddc6c239686602d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-11-26 13:08:57 +0100

    Update SafeAppsDashboardSection.tsx

--- src/components/dashboard/SafeAppsDashboardSection/SafeAppsDashboardSection.tsx
+++ src/components/dashboard/SafeAppsDashboardSection/SafeAppsDashboardSection.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 
-import { WidgetContainer } from '../styled'
+import { Card, WidgetContainer } from '../styled'
 import { AppCard } from '@/components/safe-apps/FeaturedAppCard'
 import { useSafeApps } from '@/hooks/safe-apps/useSafeApps'
 
@@ -14,11 +14,11 @@ const SafeAppsDashboardSection = () => {
       <Grid container spacing={3}>
         {rankedSafeApps.map((rankedSafeApp) => (
           <Grid key={rankedSafeApp.id} item xs={12} sm={6} md={3} xl={3}>
-            <AppCardContainer>
+            <Card>
               <Typography variant="h5">
                 Use {rankedSafeApp.name}
               </Typography>
-             </AppCardContainer>
+             </Card>
           </Grid>
         ))}
       </Grid>
