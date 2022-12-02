9b90b58d49e2c89bbcc8d6cd4f577eaf05427ae9
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-12-02 16:33:58 +0100

    Create discoverbanner.tsx

---
+++ src/components/dashboard/Banner/discoverbanner.tsx
@@ -0,0 +1,28 @@
+import type { ReactElement } from 'react'
+import { Box, Typography } from '@mui/material'
+import { Card, WidgetBody, WidgetContainer } from '../styled'
+
+const Overview = (): ReactElement => {
+
+  return (
+    <WidgetContainer>
+      <WidgetBody>
+          <Card>            
+            <Box mt={2} mb={4}>
+                <Typography fontWeight={500} mb={2}>
+        Discover a collaborative and social Safe interface
+                </Typography>
+            </Box> 
+            <Box mt={2} mb={4}>
+                <Typography fontWeight={500} mb={2}>
+Your news feed helps you keep up with recent on repositories you watch or star and people.
+                </Typography>
+            </Box> 
+          </Card>
+        )}
+      </WidgetBody>
+    </WidgetContainer>
+  )
+}
+
+export default Overview
