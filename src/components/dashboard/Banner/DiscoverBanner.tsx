294c7eff3099f60d71c4165f656c99d3ba53f2e6
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-12-02 19:55:15 +0100

    Update DiscoverBanner.tsx

--- src/components/dashboard/Banner/DiscoverBanner.tsx
+++ src/components/dashboard/Banner/DiscoverBanner.tsx
@@ -2,12 +2,18 @@ import type { ReactElement } from 'react'
 import { Box, Typography } from '@mui/material'
 import { Card, WidgetBody, WidgetContainer } from '../styled'
 
+const StyledCard = styled(Card)`
+    background-image: url(https://github.githubassets.com/images/modules/memexes/projects-beta-banner-dark.png);
+    background-size: cover;
+    box-shadow: 0 3px 6px #010409;
+`
+
 const DiscoverBanner = (): ReactElement => {
 
   return (
     <WidgetContainer>
       <WidgetBody>
-          <Card>            
+          <StyledCard>            
             <Box mt={2} mb={4}>
                 <Typography fontWeight={500} mb={2}>
         Discover a collaborative and social Safe interface
@@ -18,7 +24,7 @@ const DiscoverBanner = (): ReactElement => {
 Your news feed helps you keep up with recent on repositories you watch or star and people.
                 </Typography>
             </Box> 
-          </Card>
+          </StyledCard>
       </WidgetBody>
     </WidgetContainer>
   )
