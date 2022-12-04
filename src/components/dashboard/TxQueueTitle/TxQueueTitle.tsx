035a4ace4120c097f2e4d3c15df3bca8cdcedc1b
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-12-04 13:29:52 +0100

    Update TxQueueTitle.tsx

--- src/components/dashboard/TxQueueTitle/TxQueueTitle.tsx
+++ src/components/dashboard/TxQueueTitle/TxQueueTitle.tsx
@@ -3,11 +3,8 @@ import { Typography } from '@mui/material'
 import { WidgetContainer } from '../styled'
 
 import { getQueuedTransactionCount } from '@/utils/transactions'
-import useTxQueue from '@/hooks/useTxQueue'
 
-const { page } = useTxQueue()
-
-const totalQueuedTxs = getQueuedTransactionCount(page)
+const totalQueuedTxs = getQueuedTransactionCount
 
 const TxQueueTitle = (): ReactElement => {
 
