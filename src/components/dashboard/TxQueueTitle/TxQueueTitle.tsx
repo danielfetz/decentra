54825d9b28e7ec8a4a65deec7c0fbf1fa4b7a639
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-12-04 13:34:04 +0100

    Update TxQueueTitle.tsx

--- src/components/dashboard/TxQueueTitle/TxQueueTitle.tsx
+++ src/components/dashboard/TxQueueTitle/TxQueueTitle.tsx
@@ -2,16 +2,12 @@ import type { ReactElement } from 'react'
 import { Typography } from '@mui/material'
 import { WidgetContainer } from '../styled'
 
-import { getQueuedTransactionCount } from '@/utils/transactions'
-
-const totalQueuedTxs = getQueuedTransactionCount
-
 const TxQueueTitle = (): ReactElement => {
 
   return (
     <WidgetContainer>
         <Typography component="h2" variant="subtitle1" fontWeight={700} mb={2}>
-          Transaction queue {totalQueuedTxs ? ` (${totalQueuedTxs})` : ''}
+          Transaction queue
         </Typography>
     </WidgetContainer>
   )
