6a5187b74dbe18262c0b23d9150ad4fc69e6d01c
danielfetz <118893474+danielfetz@users.noreply.github.com>
2022-12-20 18:25:11 +0100

    Update index.tsx

--- src/components/sidebar/OwnedSafes/index.tsx
+++ src/components/sidebar/OwnedSafes/index.tsx
@@ -10,10 +10,6 @@ const OwnedSafes = (): ReactElement | null => {
 
   return (
     <>
-      <Typography variant="body2" display="inline" color="primary.light" textAlign="center" mt={1} mb={2}>
-        Safes owned on {chain.chainName}
-      </Typography>
-
       <List sx={{ py: 0 }}>
         {allOwnedSafes?.map((address) => (
           <SafeListItem
