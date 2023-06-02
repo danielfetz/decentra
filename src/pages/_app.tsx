1de71753f53f69fe6fc47d654f0805269c207bd1
N I T E <nitegeist.dev@gmail.com>
2023-06-02 03:55:28 -0500

    Add vercel analytics (#25)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,44 @@
-import Sentry from '@/services/sentry' // needs to be imported first
-import type { ReactNode } from 'react'
-import { type ReactElement } from 'react'
-import { type AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
+import CookieBanner from '@/components/common/CookieBanner'
+import ErrorBoundary from '@/components/common/ErrorBoundary'
+import MetaTags from '@/components/common/MetaTags'
+import Notifications from '@/components/common/Notifications'
+import PageLayout from '@/components/common/PageLayout'
+import { cgwDebugStorage } from '@/components/sidebar/DebugToggle'
+import { GATEWAY_URL_PRODUCTION, GATEWAY_URL_STAGING, IS_PRODUCTION } from '@/config/constants'
+import { useInitSafeCoreSDK } from '@/hooks/coreSDK/useInitSafeCoreSDK'
+import useAdjustUrl from '@/hooks/useAdjustUrl'
+import useBeamer from '@/hooks/useBeamer'
+import { useDarkMode } from '@/hooks/useDarkMode'
+import { useInitSession } from '@/hooks/useInitSession'
+import useLoadableStores from '@/hooks/useLoadableStores'
+import { useSafeMsgTracking } from '@/hooks/useSafeMsgTracking'
+import useSafeNotifications from '@/hooks/useSafeNotifications'
+import useTxNotifications from '@/hooks/useTxNotifications'
+import useTxPendingStatuses from '@/hooks/useTxPendingStatuses'
+import { useTxTracking } from '@/hooks/useTxTracking'
+import { useInitWeb3 } from '@/hooks/wallets/useInitWeb3'
+import { useInitOnboard } from '@/hooks/wallets/useOnboard'
+import useGtm from '@/services/analytics/useGtm'
+import Sentry from '@/services/sentry'; // needs to be imported first
+import { StoreHydrator } from '@/store'
+import '@/styles/globals.css'
+import createEmotionCache from '@/utils/createEmotionCache'
+import { CacheProvider, type EmotionCache } from '@emotion/react'
 import CssBaseline from '@mui/material/CssBaseline'
 import type { Theme } from '@mui/material/styles'
 import { ThemeProvider } from '@mui/material/styles'
 import { setBaseUrl as setGatewayBaseUrl } from '@safe-global/safe-gateway-typescript-sdk'
-import { CacheProvider, type EmotionCache } from '@emotion/react'
 import { SafeThemeProvider } from '@safe-global/safe-react-components'
-import '@/styles/globals.css'
-import { IS_PRODUCTION, GATEWAY_URL_STAGING, GATEWAY_URL_PRODUCTION } from '@/config/constants'
-import { StoreHydrator } from '@/store'
-import PageLayout from '@/components/common/PageLayout'
-import useLoadableStores from '@/hooks/useLoadableStores'
-import { useInitOnboard } from '@/hooks/wallets/useOnboard'
-import { useInitWeb3 } from '@/hooks/wallets/useInitWeb3'
-import { useInitSafeCoreSDK } from '@/hooks/coreSDK/useInitSafeCoreSDK'
-import useTxNotifications from '@/hooks/useTxNotifications'
-import useSafeNotifications from '@/hooks/useSafeNotifications'
-import useTxPendingStatuses from '@/hooks/useTxPendingStatuses'
-import { useInitSession } from '@/hooks/useInitSession'
-import Notifications from '@/components/common/Notifications'
-import CookieBanner from '@/components/common/CookieBanner'
-import { useDarkMode } from '@/hooks/useDarkMode'
-import { cgwDebugStorage } from '@/components/sidebar/DebugToggle'
-import { useTxTracking } from '@/hooks/useTxTracking'
-import { useSafeMsgTracking } from '@/hooks/useSafeMsgTracking'
-import useGtm from '@/services/analytics/useGtm'
-import useBeamer from '@/hooks/useBeamer'
-import ErrorBoundary from '@/components/common/ErrorBoundary'
-import createEmotionCache from '@/utils/createEmotionCache'
-import MetaTags from '@/components/common/MetaTags'
-import useAdjustUrl from '@/hooks/useAdjustUrl'
-import { createClient, configureChains, WagmiConfig } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
+import { Analytics } from '@vercel/analytics/react'
 import { SessionProvider } from 'next-auth/react'
+import { type AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import type { ReactNode } from 'react'
+import { type ReactElement } from 'react'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
 
 const { provider, webSocketProvider } = configureChains([mainnet], [publicProvider()])
 
@@ -120,9 +121,12 @@ const WebCoreApp = ({
               <CssBaseline />
 
               <InitApp />
-                <PageLayout pathname={router.pathname}>
+              <PageLayout pathname={router.pathname}>
+                <>
                   <Component {...pageProps} key={router.query.safe?.toString()} />
-                </PageLayout>
+                  <Analytics />
+                </>
+              </PageLayout>
               <CookieBanner />
               <TermsBanner />
 
