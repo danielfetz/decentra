f2ca198107edae29bd3b74e37da4ab34a14cf527
Daniel <95552251+d3centra@users.noreply.github.com>
2023-06-02 16:01:43 +0200

    Update index.tsx

--- src/components/common/Members/index.tsx
+++ src/components/common/Members/index.tsx
@@ -1,6 +1,5 @@
 import { AddOwner } from '@/components/chat/AddOwner'
-import ellipsisAddress from '@/utils/ellipsisAddress'
-import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import { OwnerList } from '@/components/settings/owner/OwnerList'
 
@@ -16,16 +15,6 @@ const Members: React.FC<TypeMembers> = ({ members }) => {
         <AddOwner />
       </Box>
       <OwnerList />
-      <List sx={{ px: 1, pb: 2 }}>
-                {members.map((member, index) => (
-           <ListItem key={member.value}>
-             <ListItemAvatar sx={{ minWidth: 35 }}>
-               <Avatar sx={{ width: 24, height: 24 }} alt={member.value} />
-             </ListItemAvatar>
-             <ListItemText primary={ellipsisAddress(`${member.value}`)} />
-           </ListItem>
-         ))}
-      </List>
     </>
   )
 }
