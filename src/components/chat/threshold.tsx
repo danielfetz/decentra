b396ed539dbeb380eb24bab7c8f11127649da7cc
Daniel <95552251+d3centra@users.noreply.github.com>
2023-06-05 15:00:28 +0200

    Create threshold.tsx

---
+++ src/components/chat/threshold.tsx
@@ -0,0 +1,14 @@
+import { ChangeThresholdDialog } from '@/components/settings/owner/ChangeThresholdDialog'
+import { Box, Typography } from '@mui/material'
+
+export const ThresholdOverview = ({ threshold, owners }: { threshold: number; owners: number }) => {
+  return (
+    <Box sx={{ display: flex }}>
+          <Typography>
+            <b>{threshold}</b> out of <b>{owners}</b> members
+          </Typography>
+
+          {owners > 1 && <ChangeThresholdDialog />}
+    </Box>
+  )
+}
